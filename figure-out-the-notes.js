4cac2ff3ad028797d2543bc01c9c90a9cb1fd3c4
Guthemberg Teixeira <guthembergteixeira@gmail.com>
2021-04-19 10:23:50 -0400

    Daily Codewars challenge

---
+++ figure-out-the-notes.js
@@ -0,0 +1,15 @@
+//Figure Out the Notes
+//https://www.codewars.com/kata/599c20626bd8795ce900001d
+const whatNote = (string, fret) => {
+    const guitar = { 
+      'e': ['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E'],
+      'B': ['B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'],
+      'G': ['G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G'],
+      'D': ['D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D'],
+      'A': ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A'],
+      'E': ['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E']
+    }
+    while(fret > 12)
+      fret -= 12;
+    return guitar[string][fret]
+  };
