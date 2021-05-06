7387a58bc7e6d9bc5b9ada111e1920c3003855c0
Guthemberg Teixeira <guthembergteixeira@gmail.com>
2021-05-06 11:06:42 -0400

    Daily CodeWars Challenge

---
+++ remove-exclamation-marks.js
@@ -0,0 +1,5 @@
+//Remove Exclamation Marks
+//https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+function removeExclamationMarks(s) {
+    return s.split('').filter(el => { return el != '!'}).join('');
+}
