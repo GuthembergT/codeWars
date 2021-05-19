ff7d9cac02ef2e200aa851efecee9e85c5839b67
Guthemberg Teixeira <guthembergteixeira@gmail.com>
2021-05-19 00:13:14 -0400

    Daily CodeWars Challenge

---
+++ twice-as-old.js
@@ -0,0 +1,5 @@
+//Twice as Old
+//https://www.codewars.com/kata/5b853229cfde412a470000d0
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - sonYearsOld - sonYearsOld);
+  }
